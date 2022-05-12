32b356e6701acaf238df3d3d9977dc14ce2a5606
André Passos <andre.passosb@gmail.com>
2022-05-11 23:26:19 -0300

    add agendamento

--- src/views/AgendaView.vue
+++ src/views/AgendaView.vue
@@ -4,17 +4,73 @@
   </section>
   <section class="wrapper">
     <v-date-picker v-model="date" />
+    <h4 class="mt-3 selecione">Selecione na agenda</h4>
+    <div class="horarios">
+      <template
+        v-for="hours in everyNminutes(duration, open, close)"
+        :key="hours"
+      >
+        <div
+          class="horarios-atendimento"
+          :class="selectHour == hours ? 'selecionado' : ''"
+          @click="selectHour = hours"
+        >
+          {{ hours }}
+        </div>
+      </template>
+    </div>
     <br />
-    {{ date }}
+    {{ day }} / {{ month }} / {{ fullYear }}
+    <br />
+    {{ selectHour }}
   </section>
 </template>
 
 <script setup>
+import { computed } from "@vue/reactivity";
 import { ref } from "vue";
 
 const date = ref(
-  " Wed May 18 2022 00:04:26 GMT-0300 (Horário Padrão de Brasília)"
+  "Wed May 18 2022 00:04:26 GMT-0300 (Horário Padrão de Brasília)"
 );
+const selectHour = ref("");
+const open = 10;
+const close = 20;
+const duration = 30;
+
+const day = computed(() => {
+  return new Date(date.value).getDate();
+});
+const month = computed(() => {
+  return new Date(date.value).getMonth();
+});
+const fullYear = computed(() => {
+  return new Date(date.value).getFullYear();
+});
+
+var everyNminutes = (duration, open, close) => {
+  var result = [];
+  for (var hours = open; hours < close; hours++) {
+    for (var minutes = 0; minutes < 60; minutes = minutes + duration) {
+      var h = "";
+      var m = "";
+      if (hours < 10) {
+        h = "0" + hours;
+      } else {
+        h = hours;
+      }
+      if (minutes < 10) {
+        m = "0" + minutes;
+      } else {
+        m = minutes;
+      }
+      result.push(h + ":" + m);
+    }
+  }
+  return result;
+};
+
+console.log(everyNminutes(15));
 </script>
 
 <style>
@@ -30,6 +86,30 @@ const date = ref(
 }
 .vc-arrow {
   transform: translateY(-4px);
+}
+.vc-highlight {
+  background-color: #5c486a !important;
+}
+.selecionado {
+  background-color: #5c486a !important;
+  color: white;
+  border-radius: 8px;
+  margin: 0 15px;
+}
+.selecione {
+  color: #5c486a;
+}
+.horarios {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+.horarios-atendimento {
+  font-family: BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, Helvetica, Arial,
+    sans-serif;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 4px 0;
 }
 </style>
 <style scoped>
