201f6f6f5759a701146b9447feeb3672181327fe
André Passos <andre.passosb@gmail.com>
2022-05-28 17:00:58 -0300

    Add get api

--- src/services/users.js
+++ src/services/users.js
@@ -1,56 +1,9 @@
-// import { requestApi } from ".";
+import { requestApi } from ".";
 
 export default () => ({
   getAllUsers: () => {
-    // const url = "/pessoas/?format=json";
-    // const response = requestApi(url, "get");
-
-    const response = {
-      data: [
-        {
-          id: 1,
-          username: "admin",
-          cpf: "",
-          first_name: "",
-          last_name: "",
-          endereco: null,
-          nascimento: null,
-          telefone: null,
-          email: "controleatendimentos@univesp.br",
-          is_atendente: false,
-          is_gerente: false,
-          new_password: null,
-        },
-        {
-          id: 2,
-          username: "teste10",
-          cpf: "21224565907",
-          first_name: "teste10",
-          last_name: "teste10",
-          endereco: "teste10",
-          nascimento: "1979-05-15",
-          telefone: null,
-          email: "teste10@teste10.com",
-          is_atendente: false,
-          is_gerente: false,
-          new_password: null,
-        },
-        {
-          id: 3,
-          username: "andre.passos",
-          cpf: "12345678910",
-          first_name: "André",
-          last_name: "Passos",
-          endereco: "Rua 10",
-          nascimento: "1990-08-06",
-          telefone: "1799998888",
-          email: "andre@gmail.com",
-          is_atendente: false,
-          is_gerente: true,
-          new_password: null,
-        },
-      ],
-    };
+    const url = "/pessoas/?format=json";
+    const response = requestApi(url, "get");
 
     return response;
   },
